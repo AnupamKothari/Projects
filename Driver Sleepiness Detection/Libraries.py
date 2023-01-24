2d75bad83254ec89bac9ce35f9573d2328134605
AnupamKothari <102417770+AnupamKothari@users.noreply.github.com>
2023-01-25 00:01:06 +0530

    Libraries Used

---
+++ Driver Sleepiness Detection/Libraries.py
@@ -0,0 +1,11 @@
+#for calculating euclidean distance
+from scipy.spatial import distance 
+#for basic operation of conversion
+from imutils import face_utils 
+import imutils
+#for inserting alarm
+from pygame import mixer 
+#for face landmarks detection
+import dlib
+#for basic image processing functions
+import cv2
